f09f12c9a97ce52c9674a67c3bb2e6bdd98ceac4
Keyi Zhang <keyi@stanford.edu>
2020-03-19 08:50:01 -0700

    improve the test cases; the need to fix the if/case statement graph where the else clause is causing problems with the state arc extraction; need to use more complicated algorithm than simple reachable to extract the state arc

--- tests/test_fsm.cc
+++ tests/test_fsm.cc
@@ -11,7 +11,33 @@ TEST_F(GraphTest, fsm_extract_fsm1) {  // NOLINT
     EXPECT_EQ(fsm.node()->name, "Color_current_state");
 
     auto arcs = fsm.self_arc();
-    EXPECT_EQ(arcs.size(), 2);
+    // there are four state transitions
+    // Red -> Red
+    // Red -> Blue
+    // Blue -> Blue
+    // Blue ->Red
+    EXPECT_EQ(arcs.size(), 4);
+
+    EXPECT_FALSE(fsm.is_counter());
+}
+
+TEST_F(GraphTest, fsm_extract_fsm2) {  // NOLINT
+    parse("fsm2.json");
+    auto fsms = g.identify_fsms();
+    EXPECT_EQ(fsms.size(), 1);
+    auto const &fsm = fsms[0];
+
+    EXPECT_EQ(fsm.node()->name, "state");
+
+    auto arcs = fsm.self_arc();
+    // this is count two logic and implemented as an explicit FSM in chisel
+    // 0 -> 0
+    // 0 -> 1
+    // 1 -> 2
+    // 1 -> 0
+    // 2 -> 2
+    // 2 -> 0
+    EXPECT_EQ(arcs.size(), 6);
 
     EXPECT_FALSE(fsm.is_counter());
 }
